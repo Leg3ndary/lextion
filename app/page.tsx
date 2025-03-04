219fba6b93b46d77ada2415c7f1a2bb00b34d122
Ben Zhou <ben.zhou.2006@gmail.com>
2025-03-03 23:31:28 -0500

    Add content

--- app/page.tsx
+++ app/page.tsx
@@ -12,6 +12,9 @@ export default function Home() {
           height={38}
           priority
         />
+        <p>
+          Testing this
+        </p>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
